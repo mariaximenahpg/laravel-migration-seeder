df74e46f12eddc2375773e1e3d75d298f1b916da
mariaximenahpg <federico.businessrico@gmail.com>
2023-01-17 17:34:50 +0100

    first commit

---
+++ database/migrations/2023_01_17_155014_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->enum('company', ['Trenitalia', 'Italo']);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 20);
+            $table->tinyInteger('wagons_number')->unsigned();
+            $table->tinyInteger('in_time')->default(1);
+            $table->tinyInteger('cancelled')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
